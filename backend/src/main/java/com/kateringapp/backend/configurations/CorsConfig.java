a19ba3234a6c27c1f34843a5cf4095fc4cdca6e2
szlazakm <michal.szlazak77@gmail.com>
2024-10-19 22:33:55 +0200

    chore: add dummmy controller, cors config and webSecurity config

---
+++ backend/src/main/java/com/kateringapp/backend/configurations/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.kateringapp.backend.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
