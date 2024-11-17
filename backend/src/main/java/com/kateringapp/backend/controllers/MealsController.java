69213539bb83bc9a31dd643d07e2b1c67002aac1
kwiat <maciek-kwiatkowski@o2.pl>
2024-11-17 13:57:26 +0100

    merge

--- backend/src/main/java/com/kateringapp/backend/controllers/MealsController.java
+++ backend/src/main/java/com/kateringapp/backend/controllers/MealsController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/meal")
+@RequestMapping("/meal")
 public class MealsController {
 
     private final MealsService mealsService;
