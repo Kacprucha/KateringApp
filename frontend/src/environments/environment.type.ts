941777fd5b5eab07feed0788db12d76aa4987125
maciejas22 <motorosas2012@gmail.com>
2024-11-23 13:39:46 +0100

    remove redundant type

--- frontend/src/environments/environment.type.ts
+++ frontend/src/environments/environment.type.ts
@@ -1,7 +1,5 @@
-type EnvironmentName = 'development' | 'catering-firm' | 'client';
-
 export interface Environment {
   production: boolean;
   apiUrl: string;
-  environmentName: EnvironmentName;
+  environmentName: 'development' | 'catering-firm' | 'client';
 }
