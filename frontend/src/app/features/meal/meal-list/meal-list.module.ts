562adb82454381b535d8514067724f63ba4daa53
bilskik <kbilski77@gmail.com>
2024-11-26 16:21:15 +0100

    fix: add modal when cannot delete meal

--- frontend/src/app/features/meal/meal-list/meal-list.module.ts
+++ frontend/src/app/features/meal/meal-list/meal-list.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import MealListComponent from './meal-list.component';
 import { RouterModule } from '@angular/router';
 import { MealService } from '../../../services/meal/meal.service';
+import MealDeleteModal from '../meal-modal/meal-delete-modal.component';
 
 @NgModule({
-  declarations: [MealListComponent],
+  declarations: [MealListComponent, MealDeleteModal],
   imports: [CommonModule, RouterModule],
   providers: [MealService],
   exports: [MealListComponent],
