f70270170457deb4d1e46e74cacb43516e7e4954
maciejas22 <motorosas2012@gmail.com>
2024-11-26 19:38:13 +0100

    feature: add keycloack redirect

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,4 +4,6 @@ export const environment: Environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
   environmentName: 'development',
+  clientUiUrl: 'http://localhost:4200',
+  cateringFirmUiUrl: 'http://localhost:4300',
 };
--- frontend/src/keycloak-init.ts
+++ frontend/src/keycloak-init.ts
@@ -1,17 +1,36 @@
 import Keycloak from 'keycloak-js';
 import { KeycloakService } from 'keycloak-angular';
+import { UserRole } from './app/types/user-roles';
+import { environment } from './environments/environment';
 
 export function initializeKeycloak(keycloak: KeycloakService) {
-    return () =>
-      keycloak.init({
+  return () =>
+    keycloak
+      .init({
         config: {
           url: 'http://localhost:8081',
           realm: 'kateringapp',
-          clientId: 'KateringAppClient'
+          clientId: 'KateringAppClient',
         },
         initOptions: {
-            onLoad: 'login-required',
+          onLoad: 'login-required',
+        },
+      })
+      .then(() => {
+        const keycloakInstance = keycloak.getKeycloakInstance();
+        const userRoles = keycloakInstance.realmAccess?.roles;
+
+        const currentUrl = new URL(window.location.href);
+        if (userRoles?.includes(UserRole.CateringFirm)) {
+          const cateringFirmUiUrl = new URL(environment.cateringFirmUiUrl);
+          if (currentUrl.host !== cateringFirmUiUrl.host) {
+            window.location.href = cateringFirmUiUrl.href;
+          }
+        } else if (userRoles?.includes(UserRole.Client)) {
+          const clientUiUrl = new URL(environment.clientUiUrl);
+          if (currentUrl.host !== clientUiUrl.host) {
+            window.location.href = clientUiUrl.href;
+          }
         }
       });
-  }
-  
+}
