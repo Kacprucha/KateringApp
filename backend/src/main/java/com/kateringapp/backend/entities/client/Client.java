0abaeae9b6a70f36bafefe27a034b310a6a03e7e
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2024-12-08 17:35:24 +0100

    Added missing client details

--- backend/src/main/java/com/kateringapp/backend/entities/client/Client.java
+++ backend/src/main/java/com/kateringapp/backend/entities/client/Client.java
@@ -1,12 +1,29 @@
 package com.kateringapp.backend.entities.client;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 
 import java.util.UUID;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Client {
 
-    UUID id;
+    @Id
+    private UUID clientId;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String address;
 
 }
