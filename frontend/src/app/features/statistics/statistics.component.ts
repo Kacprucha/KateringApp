06fb74dd852f572fdc6cf1bc38959340caa72b75
bilskik <kbilski77@gmail.com>
2024-12-18 10:08:58 +0100

    feat [FE]: add no period to charts, fill non-existence dates

--- frontend/src/app/features/statistics/statistics.component.ts
+++ frontend/src/app/features/statistics/statistics.component.ts
@@ -4,6 +4,14 @@ import { StatisticsDTO, StatisticsService } from "../../services/statistics/stat
 import Chart from 'chart.js/auto';
 import { HttpErrorResponse, HttpResponse } from "@angular/common/http";
 
+export enum DataPeriod {
+  NO_PERIOD = 'NO PERIOD',
+  LAST_WEEK = 'LAST WEEK',
+  LAST_MONTH = 'LAST MONTH',
+  LAST_YEAR = 'LAST YEAR'
+}
+
+
 @Component({
     selector: 'app-statistics',
     templateUrl: './statistics.component.html',
@@ -11,8 +19,8 @@ import { HttpErrorResponse, HttpResponse } from "@angular/common/http";
 export class StatisticsComponent implements OnInit {
   startDate: string | null = '2024-01-01';
   endDate: string | null = null;
-  dataPeriods: string[] = ['WEEK', 'MONTH', 'YEAR'];
-  selectedPeriod = 'YEAR';
+  dataPeriods = Object.values(DataPeriod); 
+  selectedPeriod: DataPeriod = DataPeriod.LAST_YEAR; 
   maxDate: string = ''
   chart: any = []
   dates: any = []
@@ -58,7 +66,7 @@ export class StatisticsComponent implements OnInit {
     })
   }
 
-  onPeriodChange(period: string): void {
+  onPeriodChange(period: DataPeriod): void {
     this.selectedPeriod = period;
     this.chart.options.scales.x.title.text = 'Period: ' + this.selectedPeriod;
     this.generateChart()
@@ -81,7 +89,10 @@ export class StatisticsComponent implements OnInit {
     this.statisticsService.getStatistics(fromDateTimestamp, toDateTimestamp, this.selectedPeriod).subscribe({
       next: (res: StatisticsDTO[]) => {
         if(res.length !== 0) {
-          const dates = res.map(item => this.convertTimestampToDate(item.date));
+          console.log("RES: ")
+          console.log(res)
+          this.fillMissingData(res)
+          const dates = res.map(item => item.date);
           const sales = res.map(item => item.sale);
           this.updateChartData(dates, sales)
         } else {
@@ -95,6 +106,70 @@ export class StatisticsComponent implements OnInit {
         this.alert.show('Error downloading sale statistics data!', 'error');
       },
     })
+  }
+
+  fillMissingData(salesData: StatisticsDTO[]) {
+    const todayFormatted = new Date().toISOString().split('T')[0];
+
+    switch(this.selectedPeriod) {
+      case DataPeriod.NO_PERIOD:
+        if (!this.endDate) {
+          this.endDate = todayFormatted          
+        }
+        if (!this.startDate) {
+          this.startDate = '2024-01-01';
+        }
+        this.fillMissingSalesDataBasedOnPeriod(salesData, this.endDate, this.startDate)
+        break;
+      case DataPeriod.LAST_WEEK:
+        this.fillMissingSalesDataBasedOnPeriod(salesData, todayFormatted, this.generateDateOffset(7, 0, 0))
+        break
+      case DataPeriod.LAST_MONTH:
+        this.fillMissingSalesDataBasedOnPeriod(salesData, todayFormatted, this.generateDateOffset(0, 1, 0))
+        break
+      case DataPeriod.LAST_YEAR:
+        this.fillMissingSalesDataBasedOnPeriod(salesData, todayFormatted, this.generateDateOffset(0, 0, 1))
+        break
+      default:
+        console.log("Invalid period provided!")
+    } 
+  }
+
+  generateDateOffset(days: number, months: number, years: number) {
+    const date = new Date();
+    date.setDate(date.getDate() - days);
+    date.setMonth(date.getMonth() - months);
+    date.setFullYear(date.getFullYear() - years);
+    return date.toISOString().split('T')[0]; // Format YYYY-MM-DD
+  }
+
+  fillMissingSalesDataBasedOnPeriod(salesData: StatisticsDTO[], endDate: string, startDate: string) {
+    const salesMap = new Map();
+
+    salesData.forEach(item => {
+      salesMap.set(item.date, item.sale);
+    });
+  
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+  
+    while (start <= end) {
+      const formattedDate = start.toISOString().split('T')[0]; // Format YYYY-MM-DD
+  
+      if (!salesMap.has(formattedDate)) {
+        salesData.push({
+          date: formattedDate,
+          sale: 0
+        });
+      }
+
+      start.setDate(start.getDate() + 1);
+    }
+
+    salesData.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+
+    console.log(salesMap)
+    console.log(salesData)
   }
 
   updateChartData(dates: string[], sales: any): void {
--- frontend/src/app/services/statistics/statistics.service.ts
+++ frontend/src/app/services/statistics/statistics.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { environment } from "../../../environments/environment";
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { DataPeriod } from "../../features/statistics/statistics.component";
 
 export interface StatisticsDTO {
     date: string,
@@ -16,12 +17,41 @@ export interface StatisticsDTO {
 
     constructor(private http: HttpClient) {}
 
-    getStatistics(startDate: string, endDate: string, statisticsPeriod: string): Observable<StatisticsDTO[]> {
-        const params = new HttpParams()
-        .set('startDate', startDate)
-        .set('endDate', endDate)
-        .set('statisticsPeriod', statisticsPeriod);
+    getStatistics(startDate: string, endDate: string, statisticsPeriod: DataPeriod): Observable<StatisticsDTO[]> {
+        const periodParam = this.mapDataPeriodToParam(statisticsPeriod)
+
+        const params = this.buildHttpParams(startDate, endDate, periodParam);
+
         return this.http.get<StatisticsDTO[]>(this.apiUrl + '/stats', { params });
+    }
+
+    mapDataPeriodToParam(period: DataPeriod): string | null {
+      switch (period) {
+          case DataPeriod.LAST_WEEK:
+              return 'WEEK';
+          case DataPeriod.LAST_MONTH:
+              return 'MONTH';
+          case DataPeriod.LAST_YEAR:
+              return 'YEAR';
+          case DataPeriod.NO_PERIOD:
+              return null
+          default:
+              return null;
+        }
+    }
+
+    private buildHttpParams(startDate: string, endDate: string, periodParam: string | null): HttpParams {
+        if (periodParam) {
+          return new HttpParams()
+          .set('startDate', startDate)
+          .set('endDate', endDate)
+          .set('statisticsPeriod', periodParam)
+        } else {
+          return new HttpParams()
+          .set('startDate', startDate)
+          .set('endDate', endDate)
+        }
+
     }
 
   }
